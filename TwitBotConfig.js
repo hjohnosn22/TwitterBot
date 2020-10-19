e829594f79477321c686b9b58a636053bfec29f0
hjohnosn22 <71099361+hjohnosn22@users.noreply.github.com>
2020-10-19 14:26:58 -0600

    Add files via upload

---
+++ TwitBotConfig.js
@@ -0,0 +1,14 @@
+// config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ * bearer token AAAAAAAAAAAAAAAAAAAAAHE8HAEAAAAACKSoiGTpTeIWsjY6UMyt4zjKgd8%3DU0400m5J6Es7SWsk4LQCpkIHIRGomlqBudEHgNWSQU2BHoyirF
+ */
+module.exports = {
+  consumer_key: 'vkodkyVsnKwjp0W9WT5cLeLcx',  
+  consumer_secret: 'vDte4bVOP9Y06vtojPYUpZuMPv19xgOTrW1rx1rOZz70CisXP7',
+  access_token: '1297934794588737542-Bjnt2loRJm9ynT2x3V02ZEv6Fvxdcz',  
+  access_token_secret: '2o7el1sqPXL1TIDkUaPXaTFGeiZ7ZI7ZQ7II2junGO4NW'
+}
